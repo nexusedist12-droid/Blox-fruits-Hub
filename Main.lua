61ed6fdedec46c781ae447a7b81acb5572216220
Nexus12-droid <nexusedist12@gmail.com>
2026-03-09 19:48:07 -0400

    Implement Sea detection and script loading
    Load different scripts based on game place ID and notify user.

--- Main.lua
+++ Main.lua
@@ -1,3 +1,30 @@
+local place = game.PlaceId
+
+local Sea = ""
+
+if place == 2753915549 then
+Sea = "First"
+print("First Sea")
+elseif place == 4442272183 then
+Sea = "Second"
+print("Second Sea")
+elseif place == 7449423635 then
+Sea = "Third"
+print("Third Sea")
+end
+
+if Sea == "First" then
+print("Loading First Sea Script")
+end
+
+if Sea == "Second" then
+print("Loading Second Sea Script")
+end
+
+if Sea == "Third" then
+print("Loading Third Sea Script")
+end
+
 loadstring(game:HttpGet("https://gist.githubusercontent.com/nexusedist12-droid/9173d225556b32a6070c1c2e3d527d0b/raw"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/nexusedist12-droid/Blox-fruits-Hub/main/Sea.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/nexusedist12-droid/Blox-fruits-Hub/main/Teleport.lua"))()
@@ -6,6 +33,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/nexusedist12-droid/Bl
 loadstring(game:HttpGet("https://raw.githubusercontent.com/nexusedist12-droid/Blox-fruits-Hub/main/Sea%20event.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/nexusedist12-droid/Blox-fruits-Hub/main/Boos.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/nexusedist12-droid/Blox-fruits-Hub/main/Blox%20Fruits%20Hub.lua"))()
+
 print("Hub Loaded")
 
 game.StarterGui:SetCore("SendNotification",{
